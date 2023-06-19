1bebf885952e7a8fa2bd1b39c9068deb8e63b8c8
viola <viola@violas-MacBook-Air.local>
2023-06-18 23:27:09 -0400

    multiple sounds

--- src/components/Soundboard/SoundPlayer.jsx
+++ src/components/Soundboard/SoundPlayer.jsx
@@ -4,16 +4,13 @@ import { useEffect } from 'react'
 import { useAudioPlayer } from 'react-use-audio-player'
 import useInteraction from '../../custom hooks/useInteraction'
 
-export default function SoundPlayer({ file }) {
+export default function SoundPlayer({ file, options }) {
   const interacted = useInteraction()
   const audio = useAudioPlayer()
 
   useEffect(() => {
     if (interacted) {
-      audio.load(file, {
-        volume: 0.5,
-        autoplay: false,
-      })
+      audio.load(file, { ...options })
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [interacted, file])
@@ -27,4 +24,5 @@ export default function SoundPlayer({ file }) {
 
 SoundPlayer.propTypes = {
   file: PropTypes.string,
+  options: PropTypes.object,
 }
--- src/components/Soundboard/index.jsx
+++ src/components/Soundboard/index.jsx
@@ -1,19 +1,33 @@
-import { Text, Box, Heading } from '@chakra-ui/react'
+import { Text, Box, Heading, IconButton, Icon } from '@chakra-ui/react'
 import SoundPlayer from './SoundPlayer'
+import { GoMute } from 'react-icons/go'
 
 // https://www.youtube.com/watch?v=0fFUfM0u3Sk
+const CAFE = 'src/assets/audio/restaurant-ambience_binaural.mp3'
+const LIGHT_RAIN = 'src/assets/audio/mixkit-light-rain-loop-1253.wav'
+const HEAVY_RAIN = 'src/assets/audio/mixkit-intense-rain-loop-1246.wav'
 
 export default function SoundBoard() {
-  // const { colorMode, toggleColorMode } = useColorMode()
+  const defaultOptions = {
+    volume: 0.5,
+    loop: true,
+  }
+  function muteAll() {}
+
   return (
-    <Box spacing={4} justifyContent='space-between'>
-      <Heading size='sm'>Sounds</Heading>
+    <Box spacing={6} justifyContent='space-between' minWidth='20vw'>
+      <Box display='flex' justifyContent='space-between' alignItems='baseline'>
+        <Heading size='md'>Sounds</Heading>
+        <IconButton size='xs' aria-label='mute-all' variant='link' onClick={muteAll} icon={<Icon as={GoMute} />}>
+          mute all
+        </IconButton>
+      </Box>
       <Text>Cafe</Text>
-      <SoundPlayer id='cafe' file={'src/assets/audio/restaurant-ambience_binaural.mp3'} />
+      <SoundPlayer id='cafe' file={CAFE} options={defaultOptions} />
       <Text>Light Rain</Text>
-      <SoundPlayer id='lightRain' file={'src/assets/audio/mixkit-light-rain-loop-1253.wav'} />
+      <SoundPlayer id='lightRain' file={LIGHT_RAIN} options={defaultOptions} />
       <Text>Heavy Rain</Text>
-      <SoundPlayer id='heavyRain' file={'src/assets/audio/mixkit-intense-rain-loop-1246.wav'} />
+      <SoundPlayer id='heavyRain' file={HEAVY_RAIN} options={defaultOptions} />
     </Box>
   )
 }
