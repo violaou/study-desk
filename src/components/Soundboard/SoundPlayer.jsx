6ade2b3b2b14481856e0bc9d19deef8b7a5fc5df
viola <viola@violas-MacBook-Air.local>
2023-06-26 23:42:20 -0400

    slider

--- src/components/Soundboard/SoundPlayer.jsx
+++ src/components/Soundboard/SoundPlayer.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
-import { Switch } from '@chakra-ui/react'
+import { Switch, Slider, SliderTrack, SliderFilledTrack, SliderThumb, Box } from '@chakra-ui/react'
+import { MdGraphicEq } from 'react-icons/md'
 import { useEffect } from 'react'
 import { useAudioPlayer } from 'react-use-audio-player'
 import useInteraction from '../../custom hooks/useInteraction'
@@ -19,7 +20,19 @@ export default function SoundPlayer({ file, options }) {
     audio.togglePlayPause()
   }
 
-  return <Switch onChange={onClick} aria-labelledby={file} />
+  return (
+    <Box display='flex' alignItems='center'>
+      <Switch onChange={onClick} aria-labelledby={file} />
+      <Slider aria-label={file} defaultValue={100} min={0} max={100} maxW='50%'>
+        <SliderTrack>
+          <SliderFilledTrack bg='grey' />
+        </SliderTrack>
+        <SliderThumb boxSize={4}>
+          <Box color='tomato' as={MdGraphicEq} />
+        </SliderThumb>
+      </Slider>
+    </Box>
+  )
 }
 
 SoundPlayer.propTypes = {
